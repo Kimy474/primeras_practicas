7477417dc71ba9eba18c47b4b23bb576a7006f74
Kimy474 <157235200+Kimy474@users.noreply.github.com>
2024-05-17 10:11:21 -0400

    medidas

---
+++ medidas/medidas/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, dime la base de tu rectangulo");
+float bases= float.Parse(Console.ReadLine());
+
+Console.WriteLine("Ahora, dime la altura de tu rectangulo");
+float altura= float.Parse(Console.ReadLine());
+
+float area= bases * altura;
+float perimetro= (altura*altura) * bases;
+Console.WriteLine("El area de tu rectangulo es " + area);
+Console.WriteLine("Y el perimetro de tu rectangulo es " + perimetro);
+
+Console.WriteLine("Espero haberte sido de ayuda");
