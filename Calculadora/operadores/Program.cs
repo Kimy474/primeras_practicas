5101468180fa645d0a211656a6c3a7a688635c11
Kimy474 <157235200+Kimy474@users.noreply.github.com>
2024-05-17 10:01:50 -0400

    calculadora simple

---
+++ Calculadora/operadores/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, Dime un numero");
+int numero1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Bien, ahora dime otro numero");
+int numero2= int.Parse(Console.ReadLine());
+int suma=numero1 + numero2;
+
+Console.WriteLine("La suma de tus numeros es: " + suma);
+
+
+int resta=numero1 - numero2;
+
+Console.WriteLine("La resta de tus numeros  es: " + resta);
+
+int Multiplicacion= numero1 * numero2;
+
+Console.WriteLine("La multiplicacion  de tus numeros  es: " + Multiplicacion);
+
+int division=numero1 / numero2;
+
+Console.WriteLine("La divicion de tus numeros  es: " + division);
